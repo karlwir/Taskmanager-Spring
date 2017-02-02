0b56ad1f3272c9dcb4a2f7ebb5fcbf967bac6b4e
daniel <danne2170@gmail.com>
2017-02-02 12:00:15 +0100

    finished config file

--- src/main/java/se/jdr/config/Config.java
+++ src/main/java/se/jdr/config/Config.java
@@ -1,5 +1,58 @@
 package se.jdr.config;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@EnableJpaRepositories("se.jdr.repository")
+@EnableTransactionManagement
 public class Config {
 
+	@Bean
+	DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setDriverClassName("com.mysql.jdbc.Driver");
+		config.setJdbcUrl("jdbc:mysql://localhost:3306/taskmanager-spring-database?useSSL=false");
+		config.setUsername("rkd3j");
+		config.setUsername("password");
+
+		return new HikariDataSource(config);
+	}
+
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory factory) {
+		return new JpaTransactionManager(factory);
+	}
+
+	@Bean
+	JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setGenerateDdl(true);
+
+		return adapter;
+	}
+
+	@Bean
+	LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource());
+		factory.setJpaVendorAdapter(jpaVendorAdapter());
+		factory.setPackagesToScan("se.jdr.model");
+
+		return factory;
+	}
 }
