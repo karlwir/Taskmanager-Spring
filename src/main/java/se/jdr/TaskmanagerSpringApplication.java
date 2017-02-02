046016cf206ca0e8aa52fe0b365a3adcb5294601
daniel <danne2170@gmail.com>
2017-02-02 11:31:16 +0100

    #anustart

---
+++ src/main/java/se/jdr/TaskmanagerSpringApplication.java
@@ -0,0 +1,12 @@
+package se.jdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerSpringApplication.class, args);
+	}
+}
