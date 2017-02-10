056b809ec87c3b975e8df059241d82342b32f2bc
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-02-10 12:55:47 +0100

    Mockbean testing attempt

---
+++ src/test/java/se/jdr/service/UserServiceTest.java
@@ -0,0 +1,40 @@
+package se.jdr.service;
+
+import se.jdr.model.User;
+import se.jdr.config.TestConfig;
+import se.jdr.repository.UserRepository;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestConfig.class, loader = AnnotationConfigContextLoader.class)
+@SpringBootTest
+public class UserServiceTest {
+	
+	@Autowired
+	private UserService userService;
+	
+    @MockBean
+    private UserRepository userRepository;
+    
+    @MockBean
+    private User user;
+
+	@Test
+	public void canGetUserByUserId() {
+		given(userRepository.findUserByUserId("15")).willReturn(user);
+		User userFromService = userService.getUserByUserId("15");
+		assertEquals(userFromService, user);
+	}
+
+}
