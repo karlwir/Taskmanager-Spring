0c5d419f9435be5f30b7ef1ba3c839f913160f08
Robert Savela <Robert@Roberts-MacBook-Pro.local>
2017-02-13 14:19:15 +0100

    Added servicetests for team and user

--- src/test/java/se/jdr/service/TeamServiceTest.java
+++ src/test/java/se/jdr/service/TeamServiceTest.java
@@ -1,40 +1,53 @@
 package se.jdr.service;
 
+import javafx.beans.binding.When;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import se.jdr.config.TestConfig;
+import se.jdr.model.Team;
 import se.jdr.model.User;
+import se.jdr.repository.TeamRepository;
 import se.jdr.repository.UserRepository;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = TestConfig.class, loader = AnnotationConfigContextLoader.class)
-public class UserServiceTest {
-
+public class TeamServiceTest {
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
-
 	@Autowired
-	UserService userService;
-
+	TeamService teamService;
 	@MockBean
 	UserRepository userRepository;
+	@MockBean
+	TeamRepository teamRepository;
+
+
+	User user = new User("Robertasdasdasd", "roberts", "Hello", "world", true);
+	Team team = new Team("Team", true);
 
 
 	@Test
-	public void shouldThrowExceptionWhenUsernameTooShort() throws ServiceException {
+	public void shouldThrowExceptionWhenTeamIsFull() throws ServiceException {
 		expectedException.expect(ServiceException.class);
-		expectedException.expectMessage("Username is too short!");
-		User user = new User("cl1", "Robert", "Savela", "12", true);
-		userService.addOrUpdateUser(user);
+		expectedException.expectMessage("Team is full!");
+		when(teamRepository.save(team)).thenReturn(team);
+		when(userRepository.countByTeamId(team.getId())).thenReturn(12L);
+		teamService.addUserToTeam(user, team);
+
+
 	}
 
 }
