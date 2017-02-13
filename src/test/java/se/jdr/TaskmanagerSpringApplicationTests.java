26c02c58376ae34fdf40b821204d172b4feb42d2
Robert Savela <Robert@Roberts-MacBook-Pro.local>
2017-02-13 11:45:18 +0100

    added Repsoitory tests for userRepository

--- src/test/java/se/jdr/TaskmanagerSpringApplicationTests.java
+++ src/test/java/se/jdr/TaskmanagerSpringApplicationTests.java
@@ -1,49 +1,20 @@
 package se.jdr;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import se.jdr.config.TestConfig;
-import se.jdr.model.User;
-import se.jdr.repository.UserRepository;
-import se.jdr.service.ServiceException;
-import se.jdr.service.UserService;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = TestConfig.class, loader = AnnotationConfigContextLoader.class)
 public class TaskmanagerSpringApplicationTests {
 
-	@Autowired
-	UserService userService;
-
-	@MockBean
-	UserRepository userRepository;
-
-	@MockBean
-	User user;
-
-	@Test
-	public void contextLoads() throws ServiceException {
-		when(userService.getUserByUserId("15")).thenReturn(user);
-		User userfromDb = userService.getUserByUserId("15");
-		System.out.println(user);
-		assertThat(userfromDb, is(user));
-
-	}
-
+    @Test
+    public void Nothing() {
+    }
 }
