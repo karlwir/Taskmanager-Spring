8a62c3e951a51740a1447aa30ec109fd5b2ff7bf
Robert Savela <Robert@Roberts-MacBook-Pro.local>
2017-02-15 10:58:48 +0100

    Added test for findWOrkitemById

--- src/test/java/se/jdr/repository/IssueRepositoryTest.java
+++ src/test/java/se/jdr/repository/IssueRepositoryTest.java
@@ -1,5 +1,6 @@
 package se.jdr.repository;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,13 +9,34 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import se.jdr.config.TestConfig;
+import se.jdr.model.Issue;
+import se.jdr.model.WorkItem;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = TestConfig.class, loader = AnnotationConfigContextLoader.class)
 public class IssueRepositoryTest {
 
-    @MockBean
-    IssueRepository issueRepository;
+	@MockBean
+	IssueRepository issueRepository;
+
+	@Test
+	public void canFindByWorkitemId() {
+		WorkItem workItem = new WorkItem("Title", "descrption");
+		Issue issue = new Issue(workItem, "title1");
+		Issue issue1 = new Issue(workItem, "title2");
+
+		when(issueRepository.findByWorkItemId(1L)).thenReturn(Stream.of(issue,issue1).collect(Collectors.toList()));
+		List<Issue> issues = issueRepository.findByWorkItemId(1L).stream().collect(Collectors.toList());
+		assertThat(issues, hasItems(issue, issue1));
+	}
 
 }
--- src/test/java/se/jdr/repository/UserRepositoryTest.java
+++ src/test/java/se/jdr/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package se.jdr.repository;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,6 +12,7 @@ import se.jdr.config.TestConfig;
 import se.jdr.model.Team;
 import se.jdr.model.User;
 import se.jdr.service.ServiceException;
+import se.jdr.service.UserService;
 
 import java.util.Collection;
 import java.util.List;
@@ -24,70 +26,81 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = TestConfig.class, loader = AnnotationConfigContextLoader.class)
-public class UserRepositoryTest {
+public final class UserRepositoryTest {
 
-    @MockBean
-    UserRepository userRepository;
+	@Autowired
+	private UserService userService;
 
-    private String userName = "Robbe";
-    private String firstName = "Robert";
-    private String lastName = "Savela";
-    private String userid = "15";
-    private User user = new User(userName, firstName, lastName, userid, true);
+	@MockBean
+	private UserRepository userRepository;
 
-    @Test
-    public void canGetUser() throws ServiceException {
-        when(userRepository.findUserByUserId("15")).thenReturn(user);
-        User userfromDb = userRepository.findUserByUserId("15");
-        System.out.println(user);
-        assertThat(userfromDb, is(user));
-    }
+	private String userName = "Robbe";
+	private String firstName = "Robert";
+	private String lastName = "Savela";
+	private String userid = "15";
+	private User user = new User(userName, firstName, lastName, userid, true);
 
-    @Test
-    public void canGetUserByFirstName() {
-        when(userRepository.getUser(firstName, "%", "%")).thenReturn(Stream.of(user).collect(Collectors.toList()));
-        List<User> users = userRepository.getUser(firstName, "%", "%").stream().collect(Collectors.toList());
-        User userFromDb = users.get(0);
-        assertThat(user, is(userFromDb));
+	@Test
+	public void canGetUser() throws ServiceException {
+		when(userRepository.findUserByUserId("15")).thenReturn(user);
+		User userfromDb = userRepository.findUserByUserId("15");
+		System.out.println(user);
+		assertThat(userfromDb, is(user));
+	}
 
-
-    }
-
-    @Test
-    public void canGetUserByLastName() {
-        when(userRepository.getUser("%", lastName, "%")).thenReturn(Stream.of(user).collect(Collectors.toList()));
-        List<User> users = userRepository.getUser("%", lastName, "%").stream().collect(Collectors.toList());
-        User userFromDb = users.get(0);
-        assertThat(userFromDb, is(user));
-    }
-
-    @Test
-    public void canGetUserByUsername() {
-        when(userRepository.getUser("%", "%", userName)).thenReturn(Stream.of(user).collect(Collectors.toList()));
-        List<User> users = userRepository.getUser("%", "%", userName).stream().collect(Collectors.toList());
-        User userFromDb = users.get(0);
-        assertThat(userFromDb, is(user));
-    }
-
-    @Test
-    public void canGetUserInTeam() {
-        Team team = new Team("Team1", true);
-        team.setUsers(Stream.of(user).collect(Collectors.toList()));
-        when(userRepository.findByTeamId(team.getId())).thenReturn(Stream.of(user).collect(Collectors.toList()));
-        List<User> users = userRepository.findByTeamId(team.getId()).stream().collect(Collectors.toList());
-        User userFromDb = users.get(0);
-        assertThat(userFromDb, is(user));
-
-    }
+	@Test
+	public void canGetUserByFirstName() {
+		when(userRepository.getUser(firstName, "%", "%")).thenReturn(Stream.of(user).collect(Collectors.toList()));
+		List<User> users = userRepository.getUser(firstName, "%", "%").stream().collect(Collectors.toList());
+		User userFromDb = users.get(0);
+		assertThat(user, is(userFromDb));
 
 
-    @Test
-    public void CanCountUsersByTeamId() {
-        Team team = new Team("Team1", true);
-        team.setUsers(Stream.of(user).collect(Collectors.toList()));
-        when(userRepository.countByTeamId(team.getId())).thenReturn(1L);
-        Long size = userRepository.countByTeamId(team.getId());
-        assertThat(size, is(1L));
-    }
+	}
+
+	@Test
+	public void canGetUserByLastName() {
+		when(userRepository.getUser("%", lastName, "%")).thenReturn(Stream.of(user).collect(Collectors.toList()));
+		List<User> users = userRepository.getUser("%", lastName, "%").stream().collect(Collectors.toList());
+		User userFromDb = users.get(0);
+		assertThat(userFromDb, is(user));
+	}
+
+	@Test
+	public void canGetUserByUsername() {
+		when(userRepository.getUser("%", "%", userName)).thenReturn(Stream.of(user).collect(Collectors.toList()));
+		List<User> users = userRepository.getUser("%", "%", userName).stream().collect(Collectors.toList());
+		User userFromDb = users.get(0);
+		assertThat(userFromDb, is(user));
+	}
+
+	@Test
+	public void canGetUserInTeam() {
+		Team team = new Team("Team1", true);
+		team.setUsers(Stream.of(user).collect(Collectors.toList()));
+		when(userRepository.findByTeamId(team.getId())).thenReturn(Stream.of(user).collect(Collectors.toList()));
+		List<User> users = userRepository.findByTeamId(team.getId()).stream().collect(Collectors.toList());
+		User userFromDb = users.get(0);
+		assertThat(userFromDb, is(user));
+
+	}
+
+
+	@Test
+	public void CanCountUsersByTeamId() {
+		Team team = new Team("Team1", true);
+		team.setUsers(Stream.of(user).collect(Collectors.toList()));
+		when(userRepository.countByTeamId(team.getId())).thenReturn(1L);
+		Long size = userRepository.countByTeamId(team.getId());
+		assertThat(size, is(1L));
+	}
+
+	@Test
+	public void canUpdateUserName() throws ServiceException {
+		String username = "Robbitrobbi";
+		when(userRepository.save(user)).thenReturn(user);
+		userService.updateUsername(user, username);
+		assertThat(user.getUsername(), is(username));
+	}
 
 }
