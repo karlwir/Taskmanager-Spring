d212e659cba2e7ba868dd8999e7fbb7ab6806a80
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-02-21 19:00:29 +0100

    Removed unused imports

--- src/main/java/se/jdr/config/Config.java
+++ src/main/java/se/jdr/config/Config.java
@@ -6,7 +6,6 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -18,8 +17,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-
-import se.jdr.service.ServiceManager;
 
 @Configuration
 @EnableJpaRepositories("se.jdr.repository")
